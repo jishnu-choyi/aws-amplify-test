4e3793bdaf7df58b9e9d482833d0ef3fdc9aa3f0
Jishnu Choyi <c.jishnu@gmail.com>
2023-08-17 21:00:39 +0530

    add welcome

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "@aws-amplify/ui-react/styles.css";
 function App({ signOut, user }) {
     return (
         <div>
-            <h1>Hello {user.username}, Welcome</h1>
+            <h1>Hello {user.username}, Welcome!</h1>
             <button onClick={signOut}>Sign out</button>
         </div>
     );
